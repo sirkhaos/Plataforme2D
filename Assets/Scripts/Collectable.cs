97c3a5cd39f0ba5479dd5dedf7b096a21b55e888
sirkhaos <alberto.torres.carreno@gmail.com>
2019-05-17 00:01:44 -0400

    agregue las gemas

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collectable : MonoBehaviour
+{
+    private static int collectableQuantity = 0;
+    [SerializeField]
+    private Text textCollectable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            gameObject.SetActive(false);
+            collectableQuantity++;
+            textCollectable.text = collectableQuantity.ToString("000");
+        }
+    }
+}
