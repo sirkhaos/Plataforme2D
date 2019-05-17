c0e19481caba94466430369fda5e08c7b2460683
sirkhaos <alberto.torres.carreno@gmail.com>
2019-05-16 22:08:21 -0400

    termimamdo el enemigo

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,25 +6,54 @@ public class EnemyController : MonoBehaviour
 {
     public float speed;
     private float timeBeforeChange;
+    private Rigidbody2D enemyRb;
+    private SpriteRenderer enemySR;
+    private Animator enemyAnim;
+
     [SerializeField]
     private float delay = 0.5f;
 
 
-    private Rigidbody2D enemyRb;
     // Start is called before the first frame update
     void Start()
     {
         enemyRb = GetComponent<Rigidbody2D>();
+        enemySR = GetComponent<SpriteRenderer>();
+        enemyAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         enemyRb.velocity = Vector2.right * speed;
+        if (speed < 0)
+        {
+            enemySR.flipX = true;
+        }
+        else
+        {
+            enemySR.flipX = false;
+        }
+
+
         if (timeBeforeChange < Time.time)
         {
             speed *= -1;
             timeBeforeChange = Time.time + delay;
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            if (transform.position.y < collision.transform.position.y)
+            {
+                enemyAnim.SetBool("isDead", true);
+            }
+        }
+    }
+    public void DisableEnemy()
+    {
+        gameObject.SetActive(false);
     }
 }
