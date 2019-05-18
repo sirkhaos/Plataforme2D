34231ea3d4da3aaa8bf8361adae824e552da351a
sirkhaos <alberto.torres.carreno@gmail.com>
2019-05-18 00:16:35 -0400

    cambio de esena

---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    private SceneChanger changerScene;
+
+    private void Start()
+    {
+        changerScene = GameObject.Find("SceneChanger").GetComponent<SceneChanger>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            changerScene.NextScene();
+        }
+    }
+}
